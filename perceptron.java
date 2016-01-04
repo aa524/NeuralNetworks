39b1db70d4162da95fca1704d132f4766bf9977e
A A <aa524@cornell.edu>
2016-01-03 23:38:22 -0500

    Output gets set properly and getters and setters added

--- perceptron.java
+++ perceptron.java
@@ -10,18 +10,48 @@ public perceptron(ArrayRealVector inputs, ArrayRealVector weights, double bias) 
 this.inputs= inputs;
 this.weights= weights;
 this.bias=bias;
+this.output= this.calculateOutput();
+}
+
+public perceptron(boolean[] inputs, ArrayRealVector weights, double bias) {
+double[] doubleInputs= new double[inputs.length];
+for (int i=0; i<inputs.length; i++) {
+//convert array of booleans to array of doubles
+doubleInputs[i]= inputs[i]? 1:0;
+}
+
+this.inputs= new ArrayRealVector(doubleInputs);
+this.weights= weights;
+this.bias=bias;
+this.output= this.calculateOutput();
 }
 
 public static void main(String[] args) {
 
-perceptron p = new perceptron(new ArrayRealVector(new double[]{0, 1, 1}), new ArrayRealVector(new double[]{6, 2, 2}), 3);
-System.out.println(p.calculateOutput(p));
+perceptron p = new perceptron(new ArrayRealVector(new double[]{0, 1, 1}), new ArrayRealVector(new double[]{6, 2, 2}), -4);
+perceptron q = new perceptron(new ArrayRealVector(new double[]{0, 1, 1}), new ArrayRealVector(new double[]{6, 2, 2}), 3);
+perceptron r = new perceptron(new ArrayRealVector(new double[]{0, 1, 1}), new ArrayRealVector(new double[]{6, 2, 2}), 3);
+perceptron z = new perceptron(new boolean[]{p.getOutput(),q.getOutput(),r.getOutput()}, new ArrayRealVector(new double[]{6, 2, 2}), -3);
+System.out.println(z.calculateOutput());
 }
 
 
-private boolean calculateOutput(perceptron p) {
-double output= p.inputs.dotProduct(p.weights) + p.bias;
-this.output= (output <= 0) ? false: true;
+private boolean calculateOutput() {
+double output= this.inputs.dotProduct(this.weights) + this.bias;
+return this.output= (output <= 0) ? false: true;
+}
+
+//setters and getters
+private void setInputs(ArrayRealVector inputs) {
+  this.inputs= inputs;
+}
+private void setWeights(ArrayRealVector weights) {
+  this.weights=weights;
+}
+private void setBias(double bias) {
+  this.bias=bias;
+}
+private boolean getOutput() {
 return this.output;
 }
 }
